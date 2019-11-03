441f62dd605de75b3c959f1e2719f9f8ebcc434e
Pedro Miguel <pedro.miguel@live.co.uk>
2019-11-03 22:27:27 +0000

    Add tests to Countdown component

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import $ from 'jquery';
+
+import Countdown from 'Countdown';
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    // the `done` argument tells mocha that this is an asynchronous test
+    // and that it should wait until `done` is called to stop the test
+    it('should set state to started and countdown', (done) => {
+      const countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001);
+    });
+
+    it('should never set count less than zero', (done) => {
+      const countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 3001);
+    });
+  });
+});
