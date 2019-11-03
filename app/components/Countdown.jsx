a785aa7451f346038af5f4a7b669aff9f88d99a9
Pedro Miguel <pedro.miguel@live.co.uk>
2019-11-03 21:56:10 +0000

    Adds countdown functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,12 +5,35 @@ import CountdownForm from 'CountdownForm';
 
 const Countdown = React.createClass({
   getInitialState() {
-    return { count: 0 };
+    return {
+      count: 0,
+      countdownStatus: 'stopped',
+    };
+  },
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch(this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      const newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0,
+      });
+    }, 1000);
   },
 
   handleSetCoundown(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started',
     });
   },
 
